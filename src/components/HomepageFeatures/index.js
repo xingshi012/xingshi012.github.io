1309e0e1d737893516e9b7a21e5c5de67a3ada61
xingshi <xingshi0816@gmail.com>
2026-01-26 22:05:16 +0800

    Update homepage style and content

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,51 +5,41 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Docs',
-    Svg: require('@site/static/img/custom/hp-feat-docs.svg').default,
+    image: require('@site/static/img/custom/hp-feat-doc.webp').default,
     url: '/docs/intro',
     description: (
       <>
-        存放自己学过的、想学的、正在学习的一些技术，以及总结出来的文档笔记
+        文档笔记
       </>
     ),
   },
   {
-    title: 'ssn',
+    title: 'About me',
+    url: '/about',
+    image: require('@site/static/img/custom/hp-feat-me.webp').default,
+    description: (
+      <>
+        关于我
+      </>
+    ),
+  },
+  {
+    title: 'Blog',
     url: '/blog',
-    Svg: require('@site/static/img/custom/hp-feat-ssn.svg').default,
+    image: require('@site/static/img/custom/hp-feat-blog.webp').default,
     description: (
       <>
-        记录自己某时的突发奇想，碎碎念
+        博客文章
       </>
     ),
-  },
-  {
-    title: 'Articles',
-    url: '/articles',
-    Svg: require('@site/static/img/custom/hp-feat-article.svg').default,
-    description: (
-      <>
-        一些之前写过的文章归档
-      </>
-    ),
-  },
-  {
-    title: 'Think',
-    url: '/think',
-    Svg: require('@site/static/img/custom/hp-feat-think.svg').default,
-    description: (
-      <>
-        思维思考
-      </>
-    ),
-  },
+  }
 ];
 
-function Feature({Svg, title, description, url}) {
+function Feature({image, title, description, url}) {
   return (
-    <a href={ url } target='_blank' className={clsx('col col--3',styles['hpfeatCard'])}>
+    <a href={ url } target='_blank' className={clsx('col',styles['hpfeatCard'])}>
       <div className={ clsx('text--center', styles['featureSvg'])}>
-        <Svg role="img" />
+        <img src={image} role="img" alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
--- src/components/HomepageSlideshow/SlideshowData.js
+++ src/components/HomepageSlideshow/SlideshowData.js
@@ -2,7 +2,7 @@
 export const slideshowImages = [
   {
     id: 1,
-    src: 'img/custom/Liberation-Bridge.jpg',
+    src: 'img/custom/Liberation-Bridge.webp',
     alt: 'Liberation Bridge Image',
     title: 'Xingshi Blog',
     description: '一个树洞，倾听记录自己的碎碎念',
