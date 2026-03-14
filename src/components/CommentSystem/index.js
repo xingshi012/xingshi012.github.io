d984c49870b8a15fa89e02833de68a71e0ce3c71
xingshi <xingshi0816@gmail.com>
2026-03-14 16:27:57 +0800

    feat(i18n): Localize content and improve multilingual support across the site

--- src/components/CommentSystem/index.js
+++ src/components/CommentSystem/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Giscus from "@giscus/react";
 import { useColorMode } from "@docusaurus/theme-common";
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export default function CommentSystem() {
 
     const { colorMode } = useColorMode();
+    const {i18n: {currentLocale}} = useDocusaurusContext();
+    const giscusLang = currentLocale === 'zh-Hans' ? 'zh-CN' : 'en';
     
     return (
         <section className="comment-system__section"> 
@@ -21,7 +24,7 @@ export default function CommentSystem() {
             emitMetadata="0"
             inputPosition="top"
             theme={colorMode === "dark" ? "dark_protanopia" : "light_protanopia"}
-            lang="zh-CN"
+            lang={giscusLang}
             loading="lazy"
             />
         </section>
